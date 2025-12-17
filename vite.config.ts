da4ec28133d02c87be490e2cd828251fc0e79846
Gilbert Akham <88907946+gilbertakham29@users.noreply.github.com>
2025-12-18 02:12:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     define: {
       // Polyfill process.env.API_KEY for the Gemini Service
-      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+      'process.env': {},
     }
   };
 });
